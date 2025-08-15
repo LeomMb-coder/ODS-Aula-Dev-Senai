93965d1de987eb5d3df0d77f7ce41bb705565e49
Leonardo Modesto <leombernardoni@gmail.com>
2025-08-15 16:34:35 -0300

    Added the ods17.html page
    Added some javascript

---
+++ index.js
@@ -0,0 +1,37 @@
+function mostrarMensagem(ods) {
+    if (ods == 'ODS1') {
+        alert('Acabar com a pobreza em todas as suas formas, em todos os lugares. Garantir que todos tenham acesso a recursos básicos e proteção social.')
+    } else if (ods == 'ODS2') {
+        alert('Acabar com a fome, alcançar a segurança alimentar, melhorar a nutrição e promover a agricultura sustentável.')
+    } else if (ods == 'ODS3') {
+        alert('Garantir uma vida saudável e promover o bem-estar para todos, em todas as idades, incluindo o acesso a serviços de saúde de qualidade.')
+    } else if (ods == 'ODS4') {
+        alert('Garantir educação inclusiva, equitativa e de qualidade, promovendo oportunidades de aprendizado ao longo da vida para todos.')
+    } else if (ods == 'ODS5') {
+        alert('Alcançar a igualdade de gênero e empoderar todas as mulheres e meninas, combatendo discriminação e violência.')
+    } else if (ods == 'ODS6') {
+        alert('Garantir disponibilidade e gestão sustentável da água e saneamento para todos.')
+    } else if (ods == 'ODS7') {
+        alert('Garantir acesso confiável, sustentável, moderno e a preço acessível à energia para todos.')
+    } else if (ods == 'ODS8') {
+        alert('Promover crescimento econômico sustentado, inclusivo e sustentável, emprego pleno e produtivo, e trabalho decente para todos.')
+    } else if (ods == 'ODS9') {
+        alert('Construir infraestrutura resiliente, promover industrialização inclusiva e sustentável e fomentar inovação.')
+    } else if (ods == 'ODS10') {
+        alert('Reduzir desigualdades dentro dos países e entre eles, promovendo inclusão social, econômica e política.')
+    } else if (ods == 'ODS11') {
+        alert('Tornar cidades e assentamentos humanos inclusivos, seguros, resilientes e sustentáveis.')
+    } else if (ods == 'ODS12') {
+        alert('Garantir padrões de consumo e produção sustentáveis, reduzindo desperdícios e promovendo a economia circular.')
+    } else if (ods == 'ODS13') {
+        alert('Tomar medidas urgentes para combater a mudança climática e seus impactos, promovendo resiliência e adaptação.')
+    } else if (ods == 'ODS14') {
+        alert('Conservar e usar de forma sustentável os oceanos, mares e recursos marinhos para o desenvolvimento sustentável.')
+    } else if (ods == 'ODS15') {
+        alert('Proteger, recuperar e promover o uso sustentável dos ecossistemas terrestres, combater a desertificação, deter a perda de biodiversidade e conservar florestas.')
+    } else if (ods == 'ODS16') {
+        alert('Promover sociedades pacíficas e inclusivas, fornecer acesso à justiça para todos e construir instituições eficazes, responsáveis e inclusivas.')
+    } else if (ods == 'ODS17') {
+        window.location.href = 'ods17.html'
+    }
+}
